f0e1bb907ff48e996b1d567d16c358006d959fc8
theek23 <theekshanadesilva28@gmail.com>
2025-03-18 17:02:46 +0530

    warning fixed

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 // Protect Routes (Require Login)
 exports.protect = async (req, res, next) => {
     let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    if (req.headers.authorization?.startsWith("Bearer")) {
         token = req.headers.authorization.split(" ")[1];
     }
 
